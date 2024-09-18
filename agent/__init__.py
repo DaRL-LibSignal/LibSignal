cbbac5c355ca2467c754740ae18956c9cf54cfa4
Red-Pheonix <rafirafi155@gmail.com>
2024-09-18 23:45:34 +0600

    fixed maddpg_v2

--- agent/__init__.py
+++ agent/__init__.py
@@ -7,7 +7,7 @@ from .sotl import SOTLAgent
 from .frap import FRAP_DQNAgent
 from .ppo_pfrl import IPPO_pfrl
 # from .maddpg import MADDPGAgent
-# from .maddpg_v2 import MADDPGAgent
+from .maddpg_v2 import MADDPGAgent
 from .presslight import PressLightAgent
 from .fixedtime import FixedTimeAgent
 from .mplight import MPLightAgent
